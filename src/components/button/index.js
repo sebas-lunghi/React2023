edbec53c7bc4a6a6f5de0c767c5c2f68df9d78dc
sebastian lunghitano <seba_88_@hotmail.com>
2023-08-23 20:49:39 -0300

    segundo commit

---
+++ src/components/button/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Button = ({text, onClick}) => {
+  return (
+    <button onClick={onClick}>
+        <p1>
+            Haz ckick aqui  
+        </p1>
+
+    </button>
+  )
+}
+
+export default Button
